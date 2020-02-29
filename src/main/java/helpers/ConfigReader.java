1c15e46eef91fa6454e0aaf1df262c64ab99b53c
David Marcas <david.marcas@gmail.com>
2020-02-29 20:25:34 +0100

    deleted code commented

--- src/main/java/helpers/ConfigReader.java
+++ src/main/java/helpers/ConfigReader.java
@@ -30,14 +30,6 @@ public class ConfigReader {
 		}
 	}
 	
-	/*public String getRequestReportPath() {
-		return properties.getProperty("requestsReportLocation");
-	}
-	
-	public String getDistributionReportPath() {
-		return properties.getProperty("distributionReportLocation");
-	}*/
-	
 	public String getStatsReportPath() {
 		return properties.getProperty("statsReportLocation");
 	}
